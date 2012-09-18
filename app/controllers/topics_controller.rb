f75f8c66bac53c8b22946193d73ddbacb8d5d90a
Can Tonic <can@cantonic.com>
2012-09-18 05:51:13 +0200

    add user_id when creaating new topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -41,6 +41,7 @@ class TopicsController < ApplicationController
   # POST /topics.json
   def create
     @topic = Topic.new(params[:topic])
+    @topic.user_id = current_user.id
 
     respond_to do |format|
       if @topic.save
