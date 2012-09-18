4aeade9fc0db8f4274cbcb5aa95983142fd2d24d
Can Tonic <can@cantonic.com>
2012-09-18 05:50:58 +0200

    add validation to Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   attr_accessible :description, :title
+  
+  validates :title, :description, :user_id, :presence => true
+  validates :title, :uniqueness => true
 end
