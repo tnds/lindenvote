b36bb7d8ca04f73ca297c9f28029dc3a5937e57f
Can Tonic <can@cantonic.com>
2012-09-18 02:45:54 +0200

    add factory girl to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require "database_cleaner"
 require 'capybara/rspec'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
