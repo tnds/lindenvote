0d4c2175dd274ce3daab0acadca7b6c3fd47a980
Can Tonic <can@cantonic.com>
2012-09-18 02:21:06 +0200

    install devise and twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lindenvote::Application.config.secret_token = '45c600611a6c1957dadb4e5ee67ebd86004bb867b960a576fee79c19850fdc41ee6bf95a90741b442039b2391007ebd366db14ca6b347424185ce20c60dc20d4'
