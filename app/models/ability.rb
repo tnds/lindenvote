e7fcad19ab1fb9adc2d5d27e4616a2310f7228ce
Can Tonic <can@cantonic.com>
2012-09-20 21:17:33 +0200

    define cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    if user.admin?
+    user ||= User.new # guest user (not logged in)
+    if user.role == "admin"
       can :manage, :all
+    elsif user.role == "user"
+      can [:create, :read], Topic
     else
-      can :read, :all
+      can :read, Topic
     end
     #
     # The first argument to `can` is the action you are giving the user permission to do.
