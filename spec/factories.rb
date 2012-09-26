b638301d3d5ed5956acaf448ff401d09e75e2dfb
Can Tonic <can@cantonic.com>
2012-09-26 13:14:36 +0200

    add argument model tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,4 +14,17 @@ FactoryGirl.define do
     sequence(:title) {|n| "Title #{n}"}
     sequence(:description) {|n| "Test #{n}"}
   end
+  
+  factory :argument do
+    topic
+    user
+    sequence(:title) {|n| "Title #{n}"}
+    sequence(:description) {|n| "Test #{n}"}
+    factory :pro_argument do
+      sort "pro"
+    end
+    factory :contra_argument do
+      sort "contra"
+    end
+  end
 end
--- spec/models/argument_spec.rb
+++ spec/models/argument_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Argument do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    before(:each) do
+      @argument = FactoryGirl.build(:argument)
+      
+    end
+    
+    [:title, :description, :sort, :topic_id, :user_id].each do |attr|
+      it "should be invalid without #{attr}" do
+				@argument.send("#{attr}=", nil)
+				@argument.should have_at_least(1).error_on(attr)
+			end
+    end
+    
+    it "should be invalid with a wrong argument type" do
+      @argument.send("sort=", "Foo")
+			@argument.should have_at_least(1).error_on(:sort)
+    end
+  end
+  
+  describe "Custom methods" do
+    before(:each) do
+      @argument = FactoryGirl.build(:pro_argument)
+    end
+    
+    it "has a score" do
+      @argument.score.should == 0
+    end
+  end
 end
